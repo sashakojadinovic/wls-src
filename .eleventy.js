cb7cbacfe5416e562b0d84479b8db9ba1eeacf90
sasa <sasa.kojadinovic@lobbster.co>
2025-07-01 20:45:09 +0200

    what-is-chemistry

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
   return {
     dir: {
       input: "src",      // odakle uzimaš sadržaj
